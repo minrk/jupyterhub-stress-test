bf13a3db8288d97b4c1986c386477d4704fc1ddd
Min RK <benjaminrk@gmail.com>
2020-11-19 10:45:01 +0100

    setup in /io/venv

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 sudo apt update
 sudo apt install -y python3-venv postgresql-client docker.io npm
-python3 -m venv ~/venv
-source ~/venv/bin/activate
+python3 -m venv /io/venv
+source /io/venv/bin/activate
 pip install -r requirements.txt
 sudo npm i -g configurable-http-proxy
--- scripts/start-jupyterhub.sh
+++ scripts/start-jupyterhub.sh
@@ -1,3 +1,10 @@
-source ~/venv/bin/activate
-source ./env
+source /io/venv/bin/activate
+source /io/.env
+if [[ "$@" == *postgres* ]]; then
+  # reset docker db
+  bash /io/docker-db.sh
+fi
+# clear sqlite before starting
+test -f jupyterhub.sqlite && rm -vf jupyterhub.sqlite
+
 exec jupyterhub "$@"
